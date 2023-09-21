b12ae79bee84504373face630bc0405d82940da5
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2023-09-21 19:08:30 +0200

    Remove Vulkan Pipeline

--- DeepEngine/VulkanPrototype/VulkanPrototype.h
+++ DeepEngine/VulkanPrototype/VulkanPrototype.h
@@ -8,41 +8,6 @@
 
 namespace DeepEngine::Renderer
 {
-
-    class VulkanPipeline
-    {
-    public:
-        VulkanPipeline()
-        {
-            CreateGraphicsPipeline();
-        }
-
-    private:
-        static std::vector<char> ReadFile(const char* p_filepath)
-        {
-            std::ifstream file { p_filepath, std::ios::ate | std::ios::binary  };
-
-            if (!file.is_open())
-            {
-                ENGINE_ERR("Failed to open \"{0}\"", p_filepath);
-                return std::vector<char>();
-            }
-
-            size_t fileSize = static_cast<size_t>(file.tellg());
-            std::vector<char> buffer(fileSize);
-
-            file.seekg(0);
-            file.read(buffer.data(), fileSize);
-            file.close();
-            return buffer;
-        }
-        void CreateGraphicsPipeline()
-        {
-            ReadFile("simple_shader.vert.spv");
-            ReadFile("simple_shader.frag.spv");
-        }
-    };
-
     class VulkanPrototype : public Core::Architecture::EngineSubsystem
     {
     public:
