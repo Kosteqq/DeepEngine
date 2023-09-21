c7feb3d2ba51ab6364c4023e4ce92e8452e5d3cf
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2023-09-21 23:01:07 +0200

    Add vulkan validation layers

--- DeepEngine/VulkanPrototype/VulkanPrototype.h
+++ DeepEngine/VulkanPrototype/VulkanPrototype.h
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <vulkan/vulkan.h>
 
+#include <fmt/format.h>
+
 #include "Debugs/Logger.h"
 #include "Architecture/EngineSystem.h"
 #include "GLFW/glfw3.h"
@@ -34,8 +36,15 @@ namespace DeepEngine::Renderer
             }
 
             FULFIL_MILESTONE(InitializeValidationLayerMS);
+
+            auto debugCreateInfo = CreateDebugInfo();
             
-            if (!CreateVulkanInstance())
+            if (!CreateVulkanInstance(debugCreateInfo))
+            {
+                return false;
+            }
+
+            if (!InitCallbackMessenger(debugCreateInfo))
             {
                 return false;
             }
@@ -53,12 +62,13 @@ namespace DeepEngine::Renderer
         {
             if (_initialized)
             {
+                DestroyDebugUtilsMessengerEXT(_instance, _debugMessenger, nullptr);
                 vkDestroyInstance(_instance, nullptr);
             }
         }
 
     private:
-        bool CreateVulkanInstance()
+        bool CreateVulkanInstance(const VkDebugUtilsMessengerCreateInfoEXT& p_debugInfo)
         {
             GetExtensions();
 
@@ -68,6 +78,14 @@ namespace DeepEngine::Renderer
                 ENGINE_ERR("GLFW requred extensions all not available");
                 return false;
             }
+
+            if (!IsExtensionAvailable(VK_EXT_DEBUG_UTILS_EXTENSION_NAME))
+            {
+                FAIL_MILESTONE(CreateVkInstanceMS);
+                ENGINE_ERR("Extension VK_EXT_DEBUG_UTILS_EXTENSION_NAME is not supported!");
+                return false;
+            }
+            EnableExtension(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
             VkApplicationInfo appInfo { };
             appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -90,6 +108,7 @@ namespace DeepEngine::Renderer
             // instanceCreateInfo.ppEnabledLayerNames = _validationLayers.data();
             instanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(_validationLayers.size());
             instanceCreateInfo.ppEnabledLayerNames = _validationLayers.data();
+            instanceCreateInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&p_debugInfo;
 
             if (vkCreateInstance(&instanceCreateInfo, nullptr, &_instance) != VK_SUCCESS)
             {
@@ -160,11 +179,10 @@ namespace DeepEngine::Renderer
             {
                 if (strcmp(_availableExtensions[i].extensionName, p_extensionName) == 0)
                 {
+                    _enabledExtensions.push_back(p_extensionName);
                     return;
                 }
             }
-
-            _enabledExtensions.push_back(p_extensionName);
         }
 
         bool IsExtensionAvailable(const char* p_extensionName)
@@ -180,6 +198,7 @@ namespace DeepEngine::Renderer
             return false;
         }
 
+    private:
         bool IsLayerAvailable(const char* p_layerName)
         {
             for (int i = 0; i < _availableLayers.size(); i++)
@@ -208,6 +227,106 @@ namespace DeepEngine::Renderer
                 DEBUG("\t{0}: {1}", _availableLayers[i].layerName, _availableLayers[i].specVersion);
             }
         }
+
+        VkDebugUtilsMessengerCreateInfoEXT CreateDebugInfo()
+        {
+            VkDebugUtilsMessengerCreateInfoEXT createInfo { };
+            createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+            createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT
+                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
+                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+            createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
+                | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
+                | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+            createInfo.pfnUserCallback = VulkanDebugCallback;
+            createInfo.pUserData = nullptr;
+            return createInfo;
+        }
+
+        bool InitCallbackMessenger(const VkDebugUtilsMessengerCreateInfoEXT& p_debugInfo)
+        {
+            if (CreateDebugUtilsMessengerEXT(_instance, &p_debugInfo, nullptr, &_debugMessenger) != VK_SUCCESS)
+            {
+                ERR("Failed to create callback messegner func");
+                FAIL_MILESTONE(InitializeValidationLayerMS);
+                return false;
+            }
+
+            FULFIL_MILESTONE(InitializeValidationLayerMS);
+            return true;
+        }
+
+        VkResult CreateDebugUtilsMessengerEXT(VkInstance p_instance, const VkDebugUtilsMessengerCreateInfoEXT* p_createInfo,
+            const VkAllocationCallbacks* p_allocator, VkDebugUtilsMessengerEXT* p_debugMessenger)
+        {
+            auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(p_instance, "vkCreateDebugUtilsMessengerEXT");
+            if (func != nullptr)
+            {
+                return func(p_instance, p_createInfo, p_allocator, p_debugMessenger);
+            }
+            
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+        
+        void DestroyDebugUtilsMessengerEXT(VkInstance p_instance, VkDebugUtilsMessengerEXT p_debugMessenger,
+            const VkAllocationCallbacks* p_allocator)
+        {
+            auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(p_instance, "vkDestroyDebugUtilsMessengerEXT");
+            
+            if (func != nullptr)
+            {
+                func(p_instance, p_debugMessenger, p_allocator);
+            }
+        }
+        
+        static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanDebugCallback(
+            VkDebugUtilsMessageSeverityFlagBitsEXT p_messageSeverity,
+            VkDebugUtilsMessageSeverityFlagsEXT p_messageType,
+            const VkDebugUtilsMessengerCallbackDataEXT* p_callbackData,
+            void* p_usePtr)
+        {
+            #define CALLBACK_FORMAT "({0}) {1}"
+            std::string messageType;
+            switch (p_messageType)
+            {
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT:
+                messageType = "low-impact";
+                break;
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT:
+                messageType = "validation";
+                break;
+            case VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT:
+                messageType = "performance";
+                break;
+            }
+            
+            if (p_messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
+            {
+                ENGINE_TRACE(CALLBACK_FORMAT, messageType, p_callbackData->pMessage);
+                return VK_FALSE;
+            }
+            if (p_messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
+            {
+                LOG_DEBUG(_debugVkLogger, CALLBACK_FORMAT, messageType, p_callbackData->pMessage);
+                return VK_FALSE;
+            }
+            if (p_messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+            {
+                LOG_WARN(_debugVkLogger, CALLBACK_FORMAT, messageType, p_callbackData->pMessage);
+                return VK_FALSE;
+            }
+            if (p_messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+            {
+                LOG_ERR(_debugVkLogger, CALLBACK_FORMAT, messageType, p_callbackData->pMessage);
+                return VK_FALSE;
+            }
+            
+            return VK_FALSE;
+        }
+        
+        static std::shared_ptr<Core::Debug::Logger> _debugVkLogger;
+        VkDebugUtilsMessengerEXT _debugMessenger;
 
     private:
         std::vector<VkExtensionProperties> _availableExtensions;
