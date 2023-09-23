a06952b73934998028480819e2e8341137610d52
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2023-09-23 18:42:27 +0200

    Add getting subsystem

--- DeepEngine/Architecture/EngineSystem.cpp
+++ DeepEngine/Architecture/EngineSystem.cpp
@@ -2,18 +2,16 @@
 
 namespace DeepEngine::Core::Architecture
 {
-    EngineSubsystemsManager::EngineSubsystemsManager() : _subsystems(16)
+    EngineSubsystemsManager::EngineSubsystemsManager() : _subsystemsMap(16)
     {
+        _subsystems.reserve(16);
     }
 
     EngineSubsystemsManager::~EngineSubsystemsManager()
     {
         for (int i = 0; i < _subsystems.size(); i++)
         {
-            if (_subsystems[i] != nullptr)
-            {
-                _subsystems[i]->Destroy();
-            }
+            _subsystems[i]->Destroy();
         }
         
         for (int i = 0; i < _subsystems.size(); i++)
@@ -26,11 +24,6 @@ namespace DeepEngine::Core::Architecture
     {
         for (int i = 0; i < _subsystems.size(); i++)
         {
-            if (_subsystems[i] == nullptr)
-            {
-                continue;
-            }
-            
             if (!_subsystems[i]->Init())
             {
                 _subsystems[i]->_initializeMilestone.MarkFailed();
--- DeepEngine/Architecture/EngineSystem.h
+++ DeepEngine/Architecture/EngineSystem.h
@@ -3,6 +3,7 @@
 #include "Debugs/Logger.h"
 
 #include <memory>
+#include <typeindex>
 #include <type_traits>
 #include <vector>
 
@@ -23,14 +24,36 @@ namespace DeepEngine::Core::Architecture
         virtual bool Init() = 0;
         virtual void Destroy() = 0;
         virtual void Tick() = 0;
-        
+
     protected:
+        EngineSubsystemsManager* _subsystemsManager;
         std::shared_ptr<Debug::Logger> _subsystemLogger;
         Debug::InitializationMilestone _initializeMilestone;
     };
 
     class EngineSubsystemsManager
     {
+    public:
+        template <typename T>
+        struct SubsystemPtr
+        {
+            friend class EngineSubsystemsManager;
+            
+        private:
+            SubsystemPtr(T* p_ptr) : _ptr(p_ptr)
+            { }
+            
+            SubsystemPtr(SubsystemPtr& p_other) = delete;
+            SubsystemPtr(SubsystemPtr&& p_other) = delete;
+
+        public:
+            T* operator->() const
+            { return _ptr; }
+
+        private:
+            T* const _ptr;
+        };
+        
     public:
         EngineSubsystemsManager();
         ~EngineSubsystemsManager();
@@ -43,12 +66,29 @@ namespace DeepEngine::Core::Architecture
         requires std::is_base_of_v<EngineSubsystem, T>
         void CreateSubsystem(Args... p_args)
         {
-            auto newSubmodule = new T(std::forward<Args>(p_args)...);
-            _subsystems.push_back(reinterpret_cast<EngineSubsystem*>(newSubmodule));
+            auto newSubmodule = reinterpret_cast<EngineSubsystem*>(new T(std::forward<Args>(p_args)...));
+            newSubmodule->_subsystemsManager = this;
+            _subsystems.push_back((newSubmodule));
+            _subsystemsMap[typeid(T)] = newSubmodule;
+        }
+
+        template <typename T>
+        requires std::is_base_of_v<EngineSubsystem, T>
+        const SubsystemPtr<T> GetSubsystem()
+        {
+            return SubsystemPtr<T>((T*)_subsystemsMap[typeid(T)]);
+        }
+
+        template <typename T>
+        requires std::is_base_of_v<EngineSubsystem, T>
+        const SubsystemPtr<T> GetSubsystem(const std::type_index p_type)
+        {
+            return SubsystemPtr<T>((T*)_subsystemsMap[p_type]);
         }
 
     private:
         std::vector<EngineSubsystem*> _subsystems;
+        std::unordered_map<std::type_index, EngineSubsystem*> _subsystemsMap;
     };
 }
 
