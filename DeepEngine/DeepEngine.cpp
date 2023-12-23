c9e58bcad06499bbc21213d012622c7cc3609d63
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2023-12-23 16:38:28 +0100

    Create event prototype

--- DeepEngine/DeepEngine.cpp
+++ DeepEngine/DeepEngine.cpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include "Debugs/Logger.h"
 #include "Debugs/InitializationTracker.h"
 #include "Architecture/EngineSystem.h"
@@ -10,47 +12,50 @@
 #include <vector>
 #include <typeinfo>
 
-enum PublishingScope
+enum class EventScope
 {
-    INHERIT = 0,
-    LOCAL = 1,
-    GLOBAL = 2
+    LOCAL = 0,
+    GLOBAL = 1
 };
 
-class BaseEvent
+struct BusObject
 {
 public:
-    BaseEvent(const BaseEvent& p_other) = delete;
-    BaseEvent(BaseEvent&& p_other) = delete;
+    BusObject(const BusObject& p_other) = delete;
+    BusObject(BusObject&& p_other) = delete;
     
-    BaseEvent() = default;
-    virtual ~BaseEvent() = default;
+    BusObject() = default;
+    virtual ~BusObject() = default;
     
     constexpr virtual const char* GetName() const = 0;
-    constexpr virtual PublishingScope GetPublishingScope() const = 0;
-    constexpr virtual size_t GetEventTypeID() const = 0;
+    constexpr virtual EventScope GetPublishingScope() const = 0;
+    constexpr virtual size_t GetTypeID() const = 0;
 };
+
+struct BaseEvent : BusObject
+{ };
 
 #define BEGIN_GLOBAL_EVENT_DEFINITION(Name)                             \
     struct Name final : BaseEvent {                                     \
         constexpr const char* GetName() const override                  \
         { return #Name; }                                               \
-        constexpr PublishingScope GetPublishingScope() const override   \
-        { return PublishingScope::GLOBAL; }                             \
-        size_t GetEventTypeID() const override                          \
+        constexpr EventScope GetPublishingScope() const override   \
+        { return EventScope::GLOBAL; }                             \
+        size_t GetTypeID() const override                               \
         { return typeid(Name).hash_code(); }
 
 #define BEGIN_LOCAL_EVENT_DEFINITION(Name)                              \
     struct Name final : BaseEvent {                                     \
         constexpr const char* GetName() const override                  \
         { return #Name; }                                               \
-        constexpr PublishingScope GetPublishingScope() const override   \
-        { return PublishingScope::LOCAL; }                              \
-        size_t GetEventTypeID() const override                          \
+        constexpr EventScope GetPublishingScope() const override   \
+        { return EventScope::LOCAL; }                              \
+        size_t GetTypeID() const override                               \
         { return typeid(Name).hash_code(); }
 
 #define END_EVENT_DEFINITION                                            \
     };
+
 
 BEGIN_LOCAL_EVENT_DEFINITION(LocalTestEvent)
     uint32_t TestInt;
@@ -60,73 +65,136 @@ BEGIN_GLOBAL_EVENT_DEFINITION(GlobalTestEvent)
     uint32_t TestInt;
 END_EVENT_DEFINITION
 
+
 enum PublishingResult
 {
-    PASS = 0,   // just continue
-    
-    SKIP = 1 << 0,   // skipping other callbacks in listener
-    BLOCK_GLOBAL = 1 << 1, // block other listeners declared in buses above
-    BLOCK_LOCAL = 1 << 2, // block other listeners declared in local buses (same or below)
-    
-    BLOCK = SKIP | BLOCK_GLOBAL | BLOCK_LOCAL,  // just blocking
+    PASS = 0,
+    BLOCK = 1,
 };
 
-class EventBus;
 
-class BaseEventListener
+template <typename TObject>
+class BusListener
 {
 protected:
-    BaseEventListener(std::function<void(BaseEventListener*)>&& p_onDestroyCallback)
+    BusListener(std::function<void(BusListener*)>&& p_onDestroyCallback)
         : _onDestroyCallback(p_onDestroyCallback)
     { }
 
 public:
-    virtual ~BaseEventListener()
+    BusListener(const BusListener&) = delete;
+    BusListener(BusListener&&) = delete;
+    
+    virtual ~BusListener()
     {
         _onDestroyCallback(this);
     }
 
-    virtual PublishingResult Publish(const BaseEvent* p_event) = 0;
-
-    constexpr virtual size_t GetListeningEventType() const = 0;
+protected:
+    virtual PublishingResult PublishFromListeners(const TObject* p_event) = 0;
+    constexpr virtual size_t GetListeningObjectType() const = 0;
 
 private:
-    const std::function<void(BaseEventListener*)> _onDestroyCallback;
+    const std::function<void(BusListener*)> _onDestroyCallback;
 };
 
-template <typename TEvent>
-requires std::is_base_of_v<BaseEvent, TEvent>
-class EventListener final : BaseEventListener
+template <typename TObject, typename TListener>
+requires std::is_base_of_v<BusObject, TObject>
+    && std::is_base_of_v<BusListener<TObject>, TListener>
+class Bus
 {
-    friend class EventBus;
+protected:
+    Bus() = default;
+    Bus(Bus<TObject, TListener>* p_parent) : _parentBus(p_parent)
+    {
+        _listeners.reserve(32);
+        _childBuses.reserve(32);
+    }
 
 public:
-     EventListener(std::function<void(BaseEventListener*)> p_onDestroyCallback)
-         : BaseEventListener(std::move(p_onDestroyCallback))
-     {
-         _callbacks.reserve(16);
-     }
+    Bus(const Bus&) = delete;
+    Bus(Bus&&) = delete;
     
-    ~EventListener() override = default;
+    virtual ~Bus() = default;
 
-    PublishingResult Publish(const BaseEvent* p_event) override
+protected:
+    template <typename T = TListener, typename ...TArgs>
+    requires std::is_base_of_v<TListener, T>
+    constexpr std::shared_ptr<T> AddListener(TArgs... p_args)
     {
-        PublishingResult returnResult { };
-        
-        for (auto& callback : _callbacks)
+        auto ptr = std::make_shared<T>(p_args, [&](BusListener<TObject>* p_listener) { DestroyListenerHandler(p_listener); }) ;
+        _listeners.push_back(ptr.get());
+        return ptr;
+    }
+    
+    template <typename T = TListener>
+    requires std::is_base_of_v<TListener, T>
+    constexpr std::shared_ptr<T> AddListener()
+    {
+        auto ptr = std::make_shared<T>([&](BusListener<TObject>* p_listener) { DestroyListenerHandler(p_listener); }) ;
+        _listeners.push_back(ptr.get());
+        return ptr;
+    }
+    
+    constexpr const std::vector<TListener*>& GetListeners() const
+    { return _listeners; }
+    
+    constexpr Bus<TObject, TListener>* GetParentBus() const
+    { return _parentBus; }
+    
+    constexpr const std::vector<Bus<TObject, TListener>*>& GetChildBuses() const
+    { return _childBuses; }
+
+private:
+    void DestroyListenerHandler(BusListener<TObject>* p_destroyedListener)
+    {
+        for (uint32_t i = 0; i < _listeners.size(); i++)
         {
-            PublishingResult result = callback(*(const TEvent*)p_event);
-            returnResult = (PublishingResult)(returnResult | result);
-            
-            if (result & PublishingResult::SKIP)
+            if (_listeners[i] == p_destroyedListener)
             {
+                _listeners.erase(_listeners.begin() + i);
                 break;
             }
         }
-
-        return (PublishingResult)returnResult;
     }
     
+private:
+    std::vector<TListener*> _listeners;
+    Bus<TObject, TListener>* _parentBus = nullptr;
+    std::vector<Bus<TObject, TListener>*> _childBuses;
+};
+
+
+class BaseEventListener : public BusListener<BaseEvent>
+{
+    friend class EventBus;
+    
+protected:
+    BaseEventListener(std::function<void(BusListener<BaseEvent>*)> p_onDestroyCallback)
+        : BusListener(std::move(p_onDestroyCallback))
+    {
+    }
+    
+public:
+    BaseEventListener(const BaseEventListener&) = delete;
+    BaseEventListener(BaseEventListener&&) = delete;
+};
+
+
+template <typename TEvent>
+requires std::is_base_of_v<BaseEvent, TEvent>
+class EventListener final : public BaseEventListener
+{
+public:
+    EventListener(std::function<void(BusListener<BaseEvent>*)> p_onDestroyCallback)
+        : BaseEventListener(std::move(p_onDestroyCallback))
+    {
+        _callbacks.reserve(16);
+    }
+    
+    EventListener(const EventListener&) = delete;
+    EventListener(EventListener&&) = delete;
+
     template <typename TPublisher>
     constexpr void BindCallback(PublishingResult (TPublisher::*p_func)(const TEvent&), TPublisher* p_publisher)
     {
@@ -143,117 +211,135 @@ public:
         _callbacks.clear();
     }
     
-    constexpr size_t GetListeningEventType() const override
+    constexpr size_t GetListeningObjectType() const override
     { return typeid(TEvent).hash_code(); }
+
+protected:
+    PublishingResult PublishFromListeners(const BaseEvent* p_event) override
+    {
+        for (auto& callback : _callbacks)
+        {
+            PublishingResult result = callback(*(const TEvent*)p_event);
+            
+            if (result == PublishingResult::BLOCK)
+            {
+                return PublishingResult::BLOCK;
+            }
+        }
+
+        return PublishingResult::PASS;
+    }
 
 private:
     std::vector<std::function<PublishingResult(const TEvent& p_event)>> _callbacks;
 };
 
-class EventBusFactory
-{
-    EventBusFactory() = default;
-    
-public:
-    ~EventBusFactory()
-    {
-        
-    }
-    
-    template <class TCreator>
-    static EventBus* CreateChildBus()
-    {
-        
-    }
 
-private:
-    inline EventBusFactory& GetInstance() const
-    {
-        static EventBusFactory instance;
-        return instance;
-    }
-
-private:
-    std::vector<EventBus> _eventBuses;
-    static uint32_t _idCounter;
-};
-
-class EventBus
+class EventBus : public Bus<BaseEvent, BaseEventListener>
 {
 private:
-    EventBus(EventBus* p_parent)
-    {
-        
-    }
+    EventBus(EventBus* p_parent) : Bus(p_parent)
+    { }
     
 public:
-    EventBus(const std::string& p_name = "Main")
-    {
-        _listeners.reserve(64);
-    }
-    
+    EventBus() : Bus()
+    { }
     EventBus(const EventBus&) = delete;
     EventBus(EventBus&&) = delete;
-    
+
     template <typename TEvent>
-    requires std::is_base_of_v<BaseEvent, TEvent>
-    void Publish(const TEvent& p_event, PublishingScope p_scope = PublishingScope::INHERIT)
+    requires std::is_base_of_v<BusObject, TEvent>
+    std::shared_ptr<EventListener<TEvent>> CreateListener()
     {
-        TIMER("Publishing \"{}\" event", typeid(TEvent).name());
+        return AddListener<EventListener<TEvent>>();
+    }
 
-        PublishingResult result { };
-
-        for (auto listener : _listeners)
+    
+    template <typename T>
+    requires std::is_base_of_v<BaseEvent, T>
+    void Publish(const T& p_event)
+    {
+        struct PublishStack
         {
-            if (listener->GetListeningEventType() != p_event.GetEventTypeID())
+            EventBus* Bus;
+            uint32_t BusChildIndex;
+        };
+        
+        TIMER("Publishing \"{}\" event", p_event.GetName());
+        EventBus* rootBus;
+
+        switch (p_event.GetPublishingScope())
+        {
+        case EventScope::LOCAL:
+            rootBus = this;
+            break;
+        case EventScope::GLOBAL:
+            rootBus = GetTopParent(this);
+            break;
+        default:
+            ENGINE_ERR("Unhandled EventScope");
+            assert(false);
+            return;
+        }
+
+        std::stack<PublishStack> stack;
+        stack.emplace(rootBus, 0);
+        
+        while (stack.size() > 0)
+        {
+            PublishStack& stackElement = stack.top();
+            const auto& childs = stackElement.Bus->GetChildBuses();
+
+            // Publish in self listeners before move to first child bus
+            if (stackElement.BusChildIndex == 0)
+            {
+                if (PublishInListeners(stackElement.Bus, p_event) == PublishingResult::BLOCK)
+                {
+                    break;
+                }
+            }
+
+            if (stackElement.BusChildIndex >= childs.size())
+            {
+                stack.pop();
+                continue;
+            }
+
+            EventBus* nextChild = static_cast<EventBus*>(childs[stackElement.BusChildIndex]);
+            stackElement.BusChildIndex++;
+            
+            stack.emplace(nextChild, 0);
+        }
+    }
+
+private:
+    static PublishingResult PublishInListeners(const EventBus* p_bus, const BaseEvent& p_event)
+    {
+        for (BaseEventListener* listener : p_bus->GetListeners())
+        {
+            if (listener->GetListeningObjectType() != p_event.GetTypeID())
             {
                 continue;
             }
 
-            PublishingResult listenerResult = listener->Publish(&p_event);
-            result = (PublishingResult)(result | listenerResult);
-            if (listenerResult & PublishingResult::BLOCK_LOCAL)
+            if (listener->PublishFromListeners(&p_event) == PublishingResult::BLOCK)
             {
-                break;
+                return PublishingResult::BLOCK;
             }
         }
-
-        if (!(result & BLOCK_LOCAL))
-        {
-            // call sub buses
-        }
-
-        if (!(result & BLOCK_GLOBAL))
-        {
-            // call buses above
-        }
+        
+        return PublishingResult::PASS;
     }
 
-    template <typename TEvent>
-    requires std::is_base_of_v<BaseEvent, TEvent>
-    std::shared_ptr<EventListener<TEvent>> CreateListener()
+    static EventBus* GetTopParent(EventBus* p_bus)
     {
-        auto ptr = std::make_shared<EventListener<TEvent>>([&](BaseEventListener* p_listener) { DestroyListenerHandler(p_listener); }) ;
-        _listeners.push_back(ptr.get());
-        return ptr;
-    }
-
-private:
-    void DestroyListenerHandler(BaseEventListener* p_destroyedListener)
-    {
-        for (uint32_t i = 0; i < _listeners.size(); i++)
+        if (p_bus->GetParentBus() != nullptr)
         {
-            if (_listeners[i] == p_destroyedListener)
-            {
-                _listeners.erase(_listeners.begin() + i);
-                break;
-            }
+            return GetTopParent(dynamic_cast<EventBus*>(p_bus->GetParentBus()));
         }
-    }
 
-private:
-    std::vector<BaseEventListener*> _listeners;
-    uint32_t _id;
+        return p_bus;
+    }
 };
 
 PublishingResult OnTestCallback(const GlobalTestEvent& p_event)
@@ -279,10 +365,20 @@ private:
 public:
     void ListenEvent()
     {
-        auto mainBus = EventBus();
-        auto listener = mainBus.CreateListener<GlobalTestEvent>();
-       listener->BindCallback(&TestClass::OnTestCallback, this);
+       //  auto mainBus = EventBus();
+       //  auto listener = mainBus.CreateListener<GlobalTestEvent>();
+       // listener->BindCallback(&TestClass::OnTestCallback, this);
     }
+};
+
+struct DUPA
+{
+    int Test;
+};
+
+struct DUPAA : DUPA
+{
+    
 };
 
 
@@ -291,6 +387,7 @@ int main(int argc, char* argv[])
     DeepEngine::Debug::Logger::Initialize("Logs/engine.log");
     auto mainBus = EventBus();
     auto listener = mainBus.CreateListener<GlobalTestEvent>();
+    
     listener->BindCallback(OnTestCallback);
     listener->BindCallback(BlockTestCallback);
     listener->BindCallback(OnTestCallback);
@@ -298,7 +395,7 @@ int main(int argc, char* argv[])
 
     auto globalEvent = GlobalTestEvent();
     globalEvent.TestInt = 20;
-    mainBus.Publish<GlobalTestEvent>(globalEvent);
+    mainBus.Publish(globalEvent);
     
     {
         TIMER("Main");
