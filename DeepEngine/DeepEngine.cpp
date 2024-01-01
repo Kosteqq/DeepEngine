9f4a14de520ae18a8cd07c862a9385e35a9d96f9
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2024-01-01 19:43:42 +0100

    Create serializable descriptor prototype

--- DeepEngine/DeepEngine.cpp
+++ DeepEngine/DeepEngine.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <stack>
 
 #include "Debugs/Logger.h"
@@ -10,8 +11,61 @@
 
 #include "Architecture/EventBus/EventBus.h"
 
+class YamlSerializeDescription
+{
+public:
+    struct ElementMetadata
+    {
+        const char* MemberName;
+        size_t MemberOffset;
+    };
 
-int main(int argc, char* argv[])
+public:
+    constexpr virtual const char* GetName() const = 0;
+    constexpr virtual const std::vector<ElementMetadata>& GetElements() const = 0;
+};
+
+// Friend some class, add GetSerializeDescription func
+#define YAML_SERIALIZABLE(Class, ...)                                           \
+class Class## YamlSerializeDescription final : YamlSerializeDescription {       \
+    using SerializableClass = Class;                                            \
+public:                                                                         \
+    constexpr const char* GetName() const override                              \
+    { return #Class; }                                                          \
+    constexpr const std::vector<ElementMetadata>& GetElements() const override  \
+    { return _elements; }                                                       \
+private:                                                                        \
+    std::vector<ElementMetadata> _elements {                                    \
+        __VA_ARGS__                                                             \
+    };                                                                          \
+};
+
+#define SERIALIZE_MEMBER(MemberName) ElementMetadata { #MemberName, offsetof(SerializableClass, MemberName) }
+
+
+class TestConfig
+{
+    YAML_SERIALIZABLE(TestConfig,
+        SERIALIZE_MEMBER(x),
+        SERIALIZE_MEMBER(x)
+    )
+
+    void GetDescription();
+
+public:
+    void BeforeSerialize()
+    {
+        typedef void(TestConfig::*funcPointer)();
+        std::function<void(TestConfig*)> q = [w = &TestConfig::BeforeSerialize](TestConfig* dupa){ (dupa->*w)(); };
+    }
+
+private:
+    int x;
+};
+
+
+
+int main(int p_argc, char* p_argv[])
 {
     DeepEngine::Debug::Logger::Initialize("Logs/engine.log");
     auto engineEventBus = DeepEngine::Architecture::EventBus();
