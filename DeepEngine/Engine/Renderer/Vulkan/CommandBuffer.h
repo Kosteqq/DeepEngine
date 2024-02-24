32e9e6fa05f9f45b3d3884619b7193fff6c56e65
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2024-02-25 00:11:22 +0100

    Move to cpp

--- DeepEngine/Engine/Renderer/Vulkan/CommandBuffer.h
+++ DeepEngine/Engine/Renderer/Vulkan/CommandBuffer.h
@@ -52,55 +52,11 @@ namespace DeepEngine::Engine::Renderer::Vulkan
     class VulkanFactory::SubFactory<CommandBuffer2>
     {
     public:
-        static VulkanRef<CommandBuffer2> Create(VulkanRef<CommandPool2> p_commandPool, bool p_asSecondary = false)
-        {
-            VkCommandBufferAllocateInfo allocInfo { };
-            allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-            allocInfo.commandPool = p_commandPool->GetHandler();
-            allocInfo.level = p_asSecondary ? VK_COMMAND_BUFFER_LEVEL_PRIMARY : VK_COMMAND_BUFFER_LEVEL_SECONDARY;
-            allocInfo.commandBufferCount = 1;
+        static VulkanRef<CommandBuffer2> Create(VulkanRef<CommandPool2> p_commandPool, bool p_asSecondary = false);
+        static std::vector<VulkanRef<CommandBuffer2>> CreateMany(VulkanRef<CommandPool2> p_commandPool, uint32_t p_amount, bool p_asSecondary = false);
 
-            VkCommandBuffer bufferHandler;
-            vkAllocateCommandBuffers(
-                _bindFactory->_vulkanInstance.GetLogicalDevice(),
-                &allocInfo,
-                &bufferHandler);
-
-            auto bufferObject = new CommandBuffer2(bufferHandler);
-            return CreateObject(bufferObject, Terminate, p_commandPool);
-        }
-
-        static std::vector<VulkanRef<CommandBuffer2>> CreateMany(VulkanRef<CommandPool2> p_commandPool, uint32_t p_amount, bool p_asSecondary = false)
-        {
-            VkCommandBufferAllocateInfo allocInfo { };
-            allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-            allocInfo.commandPool = p_commandPool->GetHandler();
-            allocInfo.level = p_asSecondary ? VK_COMMAND_BUFFER_LEVEL_PRIMARY : VK_COMMAND_BUFFER_LEVEL_SECONDARY;
-            allocInfo.commandBufferCount = 1;
-
-            std::vector<VulkanRef<CommandBuffer2>> objects(p_amount);
-
-            for (uint32_t i = 0; i < p_amount; i++)
-            {
-                VkCommandBuffer bufferHandler;
-                vkAllocateCommandBuffers(
-                    _bindFactory->_vulkanInstance.GetLogicalDevice(),
-                    &allocInfo,
-                    &bufferHandler);
-                
-                auto bufferObject = new CommandBuffer2(bufferHandler);
-                
-                objects[i] = CreateObject(bufferObject, Terminate, p_commandPool);
-            }
-
-            return objects;
-        }
-        
     private:
-        static void Terminate(VulkanObject* p_object)
-        {
-            // ...
-        }
+        static void Terminate(VulkanObject* p_object);
     };
     
 }
