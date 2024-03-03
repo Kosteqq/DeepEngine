8015637fa3ca20b2eec75006ed98baf1e8db954a
Kosteq_ <60111744+Kosteqq@users.noreply.github.com>
2024-03-03 19:52:13 +0100

    Create pipeline layout factory

--- DeepEngine/Engine/Renderer/Vulkan/PipelineLayout.h
+++ DeepEngine/Engine/Renderer/Vulkan/PipelineLayout.h
@@ -31,5 +31,58 @@ namespace DeepEngine::Engine::Renderer::Vulkan
         const uint32_t _subPassIndex;
         RenderPass* _renderPass;
     };
+
+    class PipelineLayout2 : VulkanObject
+    {
+    public:
+        PipelineLayout2(VkPipelineLayout p_handler) : _handler(p_handler)
+        { }
+        
+        VkPipelineLayout GetHandler() const
+        { return _handler; }
+
+        operator VkPipelineLayout() const
+        { return _handler; }
+
+    private:
+        const VkPipelineLayout _handler;
+    };
+
+    template <>
+    class VulkanFactory::SubFactory<PipelineLayout2>
+    {
+    public:
+        static VulkanRef<PipelineLayout2> Create(VulkanRef<RenderPass2> p_renderPass)
+        {
+            VkPipelineLayoutCreateInfo createInfo { };
+            createInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+            createInfo.setLayoutCount = 0;
+            createInfo.pSetLayouts = nullptr;
+            createInfo.pushConstantRangeCount = 0;
+            createInfo.pPushConstantRanges = nullptr;
+
+            VkPipelineLayout handler;
+            
+            vkCreatePipelineLayout(
+                _bindFactory->_vulkanInstance.GetLogicalDevice(),
+                &createInfo,
+                nullptr,
+                &handler);
+
+            auto pipelineLayout = new PipelineLayout2(handler);
+            
+            return CreateObject(pipelineLayout, Terminate, p_renderPass);
+        }
+
+    private:
+        static void Terminate(VulkanObject* p_object)
+        {
+            vkDestroyPipelineLayout(
+                _bindFactory->_vulkanInstance.GetLogicalDevice(),
+                *(PipelineLayout2*)p_object,
+                nullptr);
+        }
+        
+    };
     
 }
